d82ea5fe2d6c102ad744839a508c2195c6b99c8d
Sahin Guvenilir <60302613+sahinguvenilir@users.noreply.github.com>
2020-09-20 20:17:21 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h3 class="p-3 text-center">Demo</h3>
+    <search v-on:SearchRequested="handleSearch"></search>
+    <display :countryData=countryData></display>
+
+  </div>
+</template>
+
+<script>
+import display from "./components/display";
+import Search from "./components/search"
+import axios from 'axios';
+import Vue from 'vue';
+import VueFilterDateFormat from 'vue-filter-date-format';
+
+Vue.use(VueFilterDateFormat);
+
+require('dotenv').config();
+
+export default {
+  name: "app",
+  components: {
+    display, Search
+  },
+  data() {
+    
+    
+    return {
+      isLoading: true,
+      countryData: [],
+
+    }
+  },
+  methods: {
+
+    
+    handleSearch (query) {
+      
+      axios.get(`https://covid-19-data.p.rapidapi.com/country`,
+
+    {
+headers: {
+      "x-rapidapi-host":"covid-19-data.p.rapidapi.com",
+    "x-rapidapi-key":"0002deebc5mshcc0aabe2fd665b9p188943jsnab519b09a82b",
+    "useQueryString":true
+    },"params":{
+    "format":"json",
+    "date-format":"YYYY-MM-DD",
+    "name":`${query}`
+
+}
+    })
+    
+      .then(response => this.countryData = response.data)
+
+    }
+    
+  
+    
+    },
+ 
+}
+</script>
---
+++ search.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="search">
+        <input v type="text" v-model="query" @keypress.enter="handleSearch" placeholder="search country"/>
+        <button v-on:click="handleSearch">Search</button>
+    </div>
+</template>
+
+    <script>
+        export default {
+data () {
+    return {
+query: ''
+    }
+},
+methods:  {
+
+    handleSearch () {
+
+        this.$emit('SearchRequested', this.query);
+
+        console.log(this.query);
+    }
+}
+
+        }
+        </script>
